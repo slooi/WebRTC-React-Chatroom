d65a14e5ad07a337629fedec345dedfb90488389
slooi <s.looi2@outlook.com>
2020-08-09 04:38:49 +1200

    Tweak scrollbar css

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -5,6 +5,30 @@ class UserList extends Component{
     render(){
         return (
             <div className="user-list">
+            <UserItem username="Bob"/>
+            <UserItem username="Sammy"/>
+            <UserItem username="Bob The farmer yeah boi thisIsThe awesome-Ist manInTheWorld"/>
+                <UserItem username="Bob"/>
+                <UserItem username="Sammy"/>
+                <UserItem username="Bob The farmer yeah boi thisIsThe awesome-Ist manInTheWorld"/>
+                <UserItem username="Bob"/>
+                <UserItem username="Sammy"/>
+                <UserItem username="Bob The farmer yeah boi thisIsThe awesome-Ist manInTheWorld"/>
+                <UserItem username="Bob"/>
+                <UserItem username="Sammy"/>
+                <UserItem username="Bob The farmer yeah boi thisIsThe awesome-Ist manInTheWorld"/>
+                <UserItem username="Bob"/>
+                <UserItem username="Sammy"/>
+                <UserItem username="Bob The farmer yeah boi thisIsThe awesome-Ist manInTheWorld"/>
+                <UserItem username="Bob"/>
+                <UserItem username="Sammy"/>
+                <UserItem username="Bob The farmer yeah boi thisIsThe awesome-Ist manInTheWorld"/>
+                <UserItem username="Bob"/>
+                <UserItem username="Sammy"/>
+                <UserItem username="Bob The farmer yeah boi thisIsThe awesome-Ist manInTheWorld"/>
+                <UserItem username="Bob"/>
+                <UserItem username="Sammy"/>
+                <UserItem username="Bob The farmer yeah boi thisIsThe awesome-Ist manInTheWorld"/>
                 <UserItem username="Bob"/>
                 <UserItem username="Sammy"/>
                 <UserItem username="Bob The farmer yeah boi thisIsThe awesome-Ist manInTheWorld"/>
