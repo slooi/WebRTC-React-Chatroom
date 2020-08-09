985f98cdddc30ca227eedcbb24afddb9486ee881
slooi <s.looi2@outlook.com>
2020-08-09 20:03:52 +1200

    Add username form toggling

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
 import UserList from './components/UserList.js'
 import MainContent from './components/MainContent.js'
 import UsernameForm from './components/UsernameForm.js'
 
-const App = () => {
+
+const App = props => {
     return (
         <div className="app">
             <UserList />
             <MainContent />
-            <UsernameForm/>
+            {!(props.username.length>0) && <UsernameForm/>}
         </div>
     )
 }
 
-export default App
+const mapStateToProps = state => {
+    return {
+        username: state.username
+    }
+}
+
+export default connect(mapStateToProps,null)(App)
